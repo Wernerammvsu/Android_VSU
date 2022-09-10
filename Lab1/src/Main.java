74c8e81420e7daf297cfe080ea432ab4350136c2
Wernerammvsu <wernerammvsu@yandex.ru>
2022-09-10 19:51:22 +0300

    Создание класса Vector

---
+++ Lab1/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Vector a = new Vector(1, 3, 5);
+        Vector b = new Vector(4, 2, -2);
+        int N = 4;
+
+        System.out.println("Длина вектора а: " + a.lenVector());
+        System.out.println("Скалярное произведение вектора а на b: " + a.scalarMultiply(b));
+        System.out.println("Векторное произведение вектора а на b: " + a.vectorMultiply(b));
+        System.out.println("Угол между векторами а и b: " + a.angle(b));
+        System.out.println("Сумма векторов а и b: " + a.sum(b));
+        System.out.println("Разность векторов а и b: " + a.difference(b));
+        System.out.println("Массив векторов " + Arrays.toString(Vector.createRandomVectors(N)));
+    }
+}
---
+++ Lab1/src/Vector.java
@@ -0,0 +1,51 @@
+public class Vector {
+    double x;
+    double y;
+    double z;
+
+    Vector(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double lenVector(){
+        return Math.pow(Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2), 0.5);
+    }
+
+    public double scalarMultiply(Vector anotherVector){
+        return this.x * anotherVector.x + this.y * anotherVector.y + this.z * anotherVector.z;
+    }
+
+    public Vector vectorMultiply(Vector anotherVector){
+        double x1 = this.y * anotherVector.z - this.z * anotherVector.y;
+        double y1 = this.z * anotherVector.x - this.x * anotherVector.z;
+        double z1 = this.x * anotherVector.y - this.y * anotherVector.x;
+        return new Vector(x1, y1, z1);
+    }
+
+    public double angle(Vector anotherVector){
+        double cos =  scalarMultiply(anotherVector) / (this.lenVector() * anotherVector.lenVector());
+        return Math.acos(cos) / Math.PI * 180 ;
+    }
+
+    public Vector sum(Vector anotherVector){
+        return new Vector(this.x + anotherVector.x, this.y + anotherVector.y, this.z + anotherVector.z);
+    }
+
+    public Vector difference(Vector anotherVector){
+        return new Vector(this.x - anotherVector.x, this.y - anotherVector.y, this.z - anotherVector.z);
+    }
+
+    public static Vector[] createRandomVectors(int N){
+        Vector[] randomVectors = new Vector[N];
+        for(int i = 0; i < N; i++) {
+            randomVectors[i] = new Vector(Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10), Math.ceil(Math.random() * 10));
+        }
+        return randomVectors;
+    }
+
+    public String toString(){
+        return "[" + this.x + ", " + this.y + ", " + this.z + "]";
+    }
+}
